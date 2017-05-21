5652a07d3065de989267bddb238b0fb2ef1a9e86
root <root@laptop>
2017-05-21 20:09:47 +0000

    Added airmon init code

--- blackshrike.py
+++ blackshrike.py
@@ -1,9 +1,23 @@
 from scapy.all import * 
 import re
+import os 
 
 target = '5c:57:1a:b8:26:d0'
 
 ap_list = []
+
+def initialize_monitor():
+   wifi_file="/proc/net/wireless"
+   if os.path.exists(wifi_file):
+        with open(wifi_file) as f:
+        for line in f.readlines():
+                if not re.search(r'Inter-',line) and not re.search(r'face',line):
+                    iface=line.split(":")[0]
+   if iface:
+        print 'Initializing monitor mode [airmon method]'
+        os.system('airmon-ng start {0}'.format(iface.strip()))
+   
+
 
 def packet_process(pkt):
    global ap_list

